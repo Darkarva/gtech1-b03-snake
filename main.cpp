29c3a8ce3487ffb81a523cf2694c508e6773b6ad
Alexandre Chabre <achabre@gaming.tech>
2022-01-31 11:31:22 +0100

    Recup code ytaki

--- main.cpp
+++ main.cpp
@@ -1,52 +1,70 @@
+#include <SDL2/SDL.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <SDL2/SDL.h>
-#include "classe.hpp"
+#include "window.hpp"
+#define FPS_LIMIT 250
 
-void ExitWithError(const char *message);
+void SDL_LimitFPS(unsigned int limit);
 
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-    SDL_Window *window = NULL;
-    SDL_Renderer *renderer = NULL;
+    unsigned int frame_limit = 0;
+    frame_limit = SDL_GetTicks() + FPS_LIMIT;
 
-    if(SDL_Init(SDL_INIT_VIDEO) != 0){
-        ExitWithError("Erreur d'initialisation de la SDL");
-    }
+    //Objects creation//
+    Window snakeWindow;
 
-    if(SDL_CreateWindowAndRenderer(800, 600, 0, &window, &renderer) != 0){
-        ExitWithError("Erreur création fenetre et rendu");
-    }
+    //Window Initialization//
+    snakeWindow.InitWin();
 
     SDL_bool program_launched = SDL_TRUE;
 
     while(program_launched){
-        SDL_Event event_quit;
 
-        while(SDL_PollEvent(&event_quit)){
-            switch(event_quit.type)
-            {
+        const Uint8* keystates = SDL_GetKeyboardState(0);
+        SDL_Event event;
+
+        while(SDL_PollEvent(&event)){
+            switch(event.type){
+
+                case  SDL_KEYDOWN:
+                    switch(event.key.keysym.sym){
+
+                        case SDLK_ESCAPE:
+                            program_launched = SDL_FALSE;
+                            break;
+
+                        default:
+                            continue;
+
+                    }
                 case SDL_QUIT:
                     program_launched = SDL_FALSE;
                     break;
-                
+
                 default:
                     break;
             }
         }
+
+        SDL_LimitFPS(frame_limit);
+        frame_limit = SDL_GetTicks() + FPS_LIMIT;
+
     }
 
-    SDL_RenderPresent(renderer);
-
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
-
-    return EXIT_SUCCESS;
+    snakeWindow.DestroyWin();
 }
 
-void ExitWithError(const char *message){
-    SDL_Log("Erreur : %s > %s\n", message, SDL_GetError());
-    SDL_Quit();
-    exit(EXIT_FAILURE);
+void SDL_LimitFPS(unsigned int limit){
+    unsigned int ticks = SDL_GetTicks();
+
+    if(limit < ticks){
+        return;
+    }
+    else if(limit > ticks + FPS_LIMIT){
+        SDL_Delay(FPS_LIMIT);
+    }
+    else{
+        SDL_Delay(limit - ticks);
+    }
 }
---
+++ snake.hpp
@@ -0,0 +1,25 @@
+#include <SDL2/SDL.h>
+#include "window.hpp"
+
+#ifndef SNAKE_HPP
+#define SNAKE_HPP
+
+#define HEAD_SIZE 40
+
+class Snake{
+    public:
+
+        Snake();
+        ~Snake();
+        void move(int direction);
+        void print();
+
+    private:
+
+        int posX = 300;
+        int posY = 300;
+        int direction = 0;
+
+};
+
+#endif
